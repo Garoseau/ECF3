5c884ed2d4c9cfb0409e48174aca004d7920cc73
garoseau <garoseau.pro@gmail.com>
2022-03-31 17:56:16 +0200

    maj navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,33 +3,40 @@ import { ReactDOM } from 'react';
 import { Link, NavLink, Route, Routes } from "react-router-dom";
 import '../App.css';
 
-// export default function PageNav(){
-// 	const [page, setPage] = useState('BASE STATE');
-// 	return(
-// 		<> 
-// 		<div>
-// 			<button onClick={() => setPage('/public/propos.html')}>BUTTON</button>
-// 			LOL :{page}
-// 		</div>
-// 		</>
-// 	)
-// }
-
-export default function Navbar() {
-	return (
-		<div>
+export default function PageNav(){
+	const [page, setPage] = useState('');
+	return(
+		<>
 		<header>
-		<div class="navbar">
+ 		<div class="navbar">
+ 			<a href="index.html"class ="navbar_text"><span>Accueil</span></a>
+ 			<a href="propos.html"class="navbar_text"><span>Propos</span></a>
+ 			<a href="résumé.html" class="navbar_text"><span>Résumé</span></a>
+ 			<a href="contact.html"class="navbar_text"><span>Contact</span></a>
+ 			<a href="https://www.millenium.org/games/game-376" target="_blank" class="navbar_text"><span>MGG Star Citizen</span></a>
+ 			<a href="img/CV_Julien_Gomes.pdf" target="_blank" class="navbar_text"><span>CV</span></a>
+ 		</div>
+ 		</header>
 
-
-			<a href="index.html"class ="navbar_text"><span>Accueil</span></a>
-			<a href="propos.html"class="navbar_text"><span>Propos</span></a>
-			<a href="résumé.html" class="navbar_text"><span>Résumé</span></a>
-			<a href="contact.html"class="navbar_text"><span>Contact</span></a>
-			<a href="https://www.millenium.org/games/game-376" target="_blank" class="navbar_text"><span>MGG Star Citizen</span></a>
-			<a href="img/CV_Julien_Gomes.pdf" target="_blank" class="navbar_text"><span>CV</span></a>
-		</div>
-		</header>
-		</div>
+		</>
 	)
 }
+
+// export default function Navbar() {
+// 	return (
+// 		<div>
+// 		<header>
+// 		<div class="navbar">
+
+
+// 			<a href="index.html"class ="navbar_text"><span>Accueil</span></a>
+// 			<a href="propos.html"class="navbar_text"><span>Propos</span></a>
+// 			<a href="résumé.html" class="navbar_text"><span>Résumé</span></a>
+// 			<a href="contact.html"class="navbar_text"><span>Contact</span></a>
+// 			<a href="https://www.millenium.org/games/game-376" target="_blank" class="navbar_text"><span>MGG Star Citizen</span></a>
+// 			<a href="img/CV_Julien_Gomes.pdf" target="_blank" class="navbar_text"><span>CV</span></a>
+// 		</div>
+// 		</header>
+// 		</div>
+// 	)
+// }
