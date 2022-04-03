6cfbaf68c1ffdfa07879932e5e99f8466faec523
Julien Gomes <garoseau.pro@gmail.com>
2022-04-03 20:38:46 +0200

    m

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -9,7 +9,7 @@ const Admin = () => {
   const [products, setProducts] = useState([]);//déclaration de l'état initialisé à un tableau vide
 
 	useEffect(() => {
-    ProdService.getProducts2() 
+    ProdService.getProducts() 
       .then(res => res.json())
       .then(data => setProducts(data))//lorsqu'il y a succès on met à jour l'état avec (data)
       .catch(err => console.error(err))//catch est exécuté lorsqu'il y a erreur
@@ -49,7 +49,7 @@ return(
 
 			{products && products.map(product => (
 			<tr key={product.id}>
-			<th>{product.auteur}</th>
+			<p>{product.auteur}</p>
 			<th>{product.Titre}</th>
 			<th>Auteur</th>
 			<th>Date de création</th>
