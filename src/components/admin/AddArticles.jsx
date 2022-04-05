38b0aed149f25714bf99afd321e88476b03380f9
Julien Gomes <garoseau.pro@gmail.com>
2022-04-05 23:13:54 +0200

    AddArticle & NavBarAdmin

--- src/components/admin/AddArticles.jsx
+++ src/components/admin/AddArticles.jsx
@@ -5,30 +5,54 @@ import ProdService from '../../services/ProdServices';
 import NavbarAdmin from './NavbarAdmin';
 
 const AddArticles = () => {
-    // const initialState = { name: "", price: 0, image: "", available: false };
-    // const [product, setProduct] = useState(initialState);
-    // const navigate = useNavigate();
+    const initialState = { titre: "", auteur: "", date: "" };
+    const [product, setProduct] = useState(initialState);
+    const navigate = useNavigate();
 		
-    // const handleChange = (e) => {
-    //     let { name, value, checked, type } = e.target;
-    //     value = type === 'checkbox' ? checked : value;
-    //     setProduct({ ...product, [name]: value });
-    //     console.log(product);
-    // }
+    const handleChange = (e) => {
+        let { name, value, checked, type } = e.target;
+        value = type === 'checkbox' ? checked : value;
+        setProduct({ ...product, [name]: value });
+        console.log(product);
+    }
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log(product);
-    //     ProductService.addProduct(product).then(data => console.log(data))
-    //                                      .catch(err=>console.error(err));
-    //     setProduct(initialState);
-    //     navigate('/admin');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(product);
+        ProdService.addProduct(product).then(data => console.log(data))
+                                         .catch(err=>console.error(err));
+        setProduct(initialState);
+        navigate('/Admin');
 
-    // }
+    }
     return (
 <>
     <NavbarAdmin></NavbarAdmin>
+    
+    <div className='box'>
+      <form onSubmit={handleSubmit}>
 
+        <div>
+          <label className='text'>Nom de l'article</label>
+          <input id='name' name='name'type="text" placeholder="Titre de l'article" value={product.titre} onChange={handleChange} />
+        </div>
+
+        <div>
+          <label className='text'>Auteur</label>
+          <input id='name' name='name'type="text" placeholder="Nom de l'auteur" value={product.auteur} onChange={handleChange} />
+        </div>
+
+        <div>
+          <label className='text'>Date</label>
+          <input id='name' name='name'type="text" placeholder="JJ/MM/AAAA" value={product.date} onChange={handleChange} />
+        </div>
+
+        <div class="">
+        <input type="submit" class="button" />
+        </div>
+
+      </form>
+    </div>
  </>
     )
 }
--- src/components/admin/NavbarAdmin.jsx
+++ src/components/admin/NavbarAdmin.jsx
@@ -19,6 +19,7 @@ function NavbarAdmin() {
 
  		<a className="gradient gB anime navbar_text" href="https://www.millenium.org/games/game-376" target="_blank">MGG Star Citizen</a>
  		<a className="gradient gB anime navbar_text" href="img/CV_Julien_Gomes.pdf" target="_blank">CV</a>
+		 <Link className='gradient gValid anime navbar_text' to="/Admin">Home Admin</Link>
 		<Link className='gradient gGold anime navbar_text' to='/AddArticles'> Add</Link>
 		<Link className='gradient gError anime navbar_text' to={`/add-product`}> Delete</Link>
 
