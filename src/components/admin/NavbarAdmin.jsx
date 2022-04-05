4fbdfe431c44e3e5093d3bd51a1f5b0cb9226f28
Julien Gomes <garoseau.pro@gmail.com>
2022-04-05 21:17:51 +0200

    Opti CSS

--- src/components/admin/NavbarAdmin.jsx
+++ src/components/admin/NavbarAdmin.jsx
@@ -2,7 +2,7 @@ import '../../App.css';
 import React from 'react'
 import { Link, Outlet,useNavigate } from "react-router-dom";
 import LogServices from "../../services/LogServices";
-
+import PowerSettingsNewRoundedIcon from '@mui/icons-material/PowerSettingsNewRounded';
 
 function NavbarAdmin() {
 	const navigate = useNavigate();
@@ -19,7 +19,8 @@ function NavbarAdmin() {
 
  		<a className="gradient gB anime navbar_text" href="https://www.millenium.org/games/game-376" target="_blank">MGG Star Citizen</a>
  		<a className="gradient gB anime navbar_text" href="img/CV_Julien_Gomes.pdf" target="_blank">CV</a>
-		<a className='gradient gB anime navbar_text' onClick={handleClick}>Logout</a>
+		<Link className='gradient gC anime navbar_text' to={`/add-product`}> Add</Link>
+		<a className='gradient gD anime navbar_text' onClick={handleClick}> Sign Out</a>
  	</div>
 
  	<Outlet />
