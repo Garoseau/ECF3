bbde4f1a91b300418cc91e4613e93c261db7ae97
Garoseau <garoseau.pro@gmail.com>
2022-05-25 11:16:24 +0200

    maj cv navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
  return (
  <>
  	<div class="navbar">
-		<a className='gradient gB anime navbar_text' href="" >Julien "Garoseau" Gomes</a>
+		<a className='gradient gB anime navbar_text' href="/public/Resume FR Julien Gomes.pdf" >Julien "Garoseau" Gomes</a>
  		<Link className='gradient gB anime navbar_text' to="/">Projects</Link>
  		<Link className='gradient gB anime navbar_text' to="/Propos">About</Link>
  		<Link className='gradient gB anime navbar_text' to="/Contact">Contact</Link>
